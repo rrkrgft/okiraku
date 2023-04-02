0186019c8bdbf3764ba5635c8956b8741176946d
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-02 20:09:46 +0900

    model作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_02_073446) do
+ActiveRecord::Schema.define(version: 2023_04_02_110816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "details", force: :cascade do |t|
+    t.text "public"
+    t.text "secret"
+    t.text "deeply"
+    t.boolean "secret_choice_deep"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_details_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "image"
+    t.integer "score"
+    t.boolean "draft"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,4 +60,6 @@ ActiveRecord::Schema.define(version: 2023_04_02_073446) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "details", "posts"
+  add_foreign_key "posts", "users"
 end
