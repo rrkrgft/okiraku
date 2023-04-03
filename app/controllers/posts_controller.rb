660355627fd59ef33cde047fb371b7190a88c9cd
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-03 19:05:59 +0900

    create実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
+    @detail = Detail.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "登録しました"
+    else
+      render :new, notice: "登録エラーです"
+    end
   end
 
   def index
@@ -10,5 +20,10 @@ class PostsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :score, :image, :draft, detail_attributes: [:public, :secret, :deeply, :secret_choice_deep, :_destroy, :id])
   end
 end
