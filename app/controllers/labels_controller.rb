fc3498ce653b902b4df54858367bb0b6495e7660
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-04 15:59:22 +0900

    コントローラーの作成とルートの設定

---
+++ app/controllers/labels_controller.rb
@@ -0,0 +1,10 @@
+class LabelsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/labels_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class LabelsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get labels_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get labels_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get labels_edit_url
+    assert_response :success
+  end
+end
