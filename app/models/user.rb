12861f2faeba9e8d2ad3ae18662c6ecae8e4c92e
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-04 15:00:33 +0900

    ユーザーにnameカラム追加、入力欄追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+         
+  validates :name, presence: true
 
   has_many :posts
   has_many :details, through: :posts
