112ecac3f557069bc8cf52bcaa1233d865d80ee0
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-04 11:51:57 +0900

    detail新規登録, 詳細画面実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
-    @detail = Detail.new
+    detail = @post.build_detail
   end
 
   def create
@@ -13,18 +13,38 @@ class PostsController < ApplicationController
     end
   end
 
+  # def create
+  #   @post = Post.create(post_params)
+  #   @post.user_id = current_user.id
+  #   if @post.save
+  #     redirect_to posts_path, notice: "登録しました"
+  #   else
+  #     render :new, notice: "登録エラーです"
+  #   end
+  # end
+
   def index
     @posts = Post.all
   end
 
   def show
+    set_post
   end
 
   def edit
+    
   end
 
   private
   def post_params
     params.require(:post).permit(:title, :score, :image, :draft, detail_attributes: [:public, :secret, :deeply, :secret_choice_deep, :_destroy, :id])
+  end
+
+  def detail_params
+    params.require(:post).permit(detail: [:public, :secret, :deeply, :secret_choice_deep, :_destroy, :id])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :draft, presence: true
+
   belongs_to :user
   has_one :detail
+  accepts_nested_attributes_for :detail, allow_destroy: true
 end
