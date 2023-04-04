fd552b909824d3dbb8f588263d5edacb4fee33cb
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-04 16:15:48 +0900

    ラベルno createの実装

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -3,11 +3,23 @@ class LabelsController < ApplicationController
     @label = Label.new
   end
 
-  
+  def create
+    @label = current_user.labels.build(label_params)
+    if @label.save
+      redirect_to labels_path, notice: "ラベルを登録しました"
+    else
+      render :new, notice: "ラベル登録のエラーです"
+    end
+  end
 
   def index
   end
 
   def edit
+  end
+
+  private
+  def label_params
+    params.require(:label).permit(:name)
   end
 end
