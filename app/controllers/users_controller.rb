784675f43a7656ef8927b9e4d1392687d1d39b4e
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-06 12:01:38 +0900

    ユーザー投稿一覧画面に検索 、並び替え実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def index
+    set_q
     @posts = Post.where(user_id: current_user.id)
+    if params[:q]
+      @posts = @q.result
+    end
+  end
+
+  private
+  def set_q
+    @q = Post.ransack(params[:q])
   end
 end
