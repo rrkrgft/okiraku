9fac864570bc8f291d1a59501288330f672740a3
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-11 20:59:05 +0900

    コメントアウトに

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-  config.web_console.whitelisted_ips = ''
+  # config.web_console.whitelisted_ips = ''
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
