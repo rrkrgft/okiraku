f250853dd34a9fcf5c53e369426a7a0e3faf47a2
kuniyuki isaji <rrkrgft@gmail.com>
2023-04-17 18:52:13 +0900

    ユーザーテストの修正終了

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe "ユーザー機能", type: :system do
       end
     end
   end
-  describe 'セッションのログイン機能' do
+  describe '一般ユーザーのログイン機能' do
     let!(:user) { FactoryBot.create(:user) }
     before do
       sign_in(user)
@@ -60,11 +60,81 @@ RSpec.describe "ユーザー機能", type: :system do
         expect(page).not_to have_content 'ログイン'
       end
     end
+    context 'ユーザーの名前とメールアドレスの編集をした場合' do
+      it 'ユーザー名、メールアドレスの編集ができる' do
+        visit edit_user_registration_path(user)
+        fill_in '名前', with: 'test_edit'
+        fill_in 'メールアドレス', with: 'test_edit@test.com'
+        find('#user_current_password').set("password")
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。'
+        visit edit_user_registration_path(user)
+        expect(page).to have_field '名前', with: 'test_edit'
+        expect(page).to have_field 'メールアドレス', with: 'test_edit@test.com'
+      end
+    end
+    context 'ユーザーの名前の編集をした場合' do
+      it 'ユーザー名の編集ができる' do
+        visit edit_user_registration_path(user)
+        fill_in '名前', with: 'test_edit'
+        find('#user_current_password').set("password")
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。'
+        visit edit_user_registration_path(user)
+        expect(page).to have_field '名前', with: 'test_edit'
+      end
+    end
+    context 'ユーザーのメールアドレスの編集をした場合' do
+      it 'メールアドレスの編集ができる' do
+        visit edit_user_registration_path(user)
+        fill_in 'メールアドレス', with: 'test_edit@test.com'
+        find('#user_current_password').set("password")
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。'
+        visit edit_user_registration_path(user)
+        expect(page).to have_field 'メールアドレス', with: 'test_edit@test.com'
+      end
+    end
+    context 'ユーザーのパスワードを変更をした場合' do
+      it 'パスワードの変更ができる' do
+        visit edit_user_registration_path(user)
+        find('#user_password').set("password1")
+        find('#user_password_confirmation').set("password1")
+        find('#user_current_password').set("password")
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。'
+        visit edit_user_registration_path(user)
+        fill_in 'メールアドレス', with: 'test_edit@test.com'
+        find('#user_current_password').set("password1")
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。'
+      end
+    end
     context 'ログアウトした場合' do
       it 'ログアウトできる' do
         click_on 'ログアウト'
         expect(page).to have_content 'ログアウトしました'
         expect(page).not_to have_content 'マイページ'
+      end
+    end
+    context '管理者画面にアクセスした場合' do
+      it 'アクセスできない' do
+        visit rails_admin_path
+        expect(page).to have_content '画面を閲覧する権限がありません。'
+        expect(page).to have_content '嬉しいこと、楽しいことの一覧'
+      end
+    end
+  end
+  describe '管理者権限の機能' do
+    let!(:user) { FactoryBot.create(:user, admin: true) }
+    before do
+      sign_in(user)
+    end
+    context '管理者ページにアクセスした場合' do
+      it 'アクセスできる' do
+        visit rails_admin_path
+        expect(page).to have_content 'サイト管理'
+        expect(page).to have_content 'ナビゲーション'
       end
     end
   end
